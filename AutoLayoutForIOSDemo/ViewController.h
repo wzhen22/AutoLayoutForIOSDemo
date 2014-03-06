3bcc0e7dcfe517c0ebf13d83517c180824c8fbe8
wzhen22 <qwaszx375204@126.com>
2014-03-05 17:26:32 -0800

    Initial Commit

---
+++ AutoLayoutForIOSDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLayoutForIOSDemo
+//
+//  Created by 传晟 on 14-3-5.
+//  Copyright (c) 2014年 传晟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
